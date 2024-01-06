9871ce37d31b4e150d37189236c3e6de3351ec69
Md Mofizul Islam (Sohan) <mofizulislam0216@gmail.com>
2024-01-06 13:45:31 +0600

    Add blog-related controllers and routes

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -4,5 +4,78 @@ const slugify = require("slugify");
 const validateMongodbId = require("../utils/validateMongodbId");
 
 // create new blog controller
+const createBlog = asyncHandler(async (req, res) => {
+  try {
+    if (req.body.title) {
+      req.body.slug = slugify(req.body.title, {
+        remove: /[*+~.()'"!:@]/g,
+        lower: true,
+        trim: true,
+      });
+    }
+    const newBlog = await Blog.create(req.body);
+    res.json({
+      success: true,
+      message: "create new blog successfully",
+      data: newBlog,
+    });
+  } catch (error) {
+    throw new Error(error.message);
+  }
+});
+
+// get all blogs controller
+const getAllBlogs = asyncHandler(async (req, res) => {
+  try {
+    const blogs = await Blog.find({});
+    res.json({
+      success: true,
+      message: "get all blogs successfully",
+      data: blogs,
+      meta: {
+        total: blogs.length,
+      },
+    });
+  } catch (error) {
+    throw new Error(error.message);
+  }
+});
+
+// get blog by id controller
+const getBlogById = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongodbId(id);
+  try {
+    const blogById = await Blog.findById(id);
+    res.json({
+      success: true,
+      message: "get blog by id successfully",
+      data: blogById,
+    });
+  } catch (error) {
+    throw new Error(error.message);
+  }
+});
+
+// get blog by slug controller
+const getBlogBySlug = asyncHandler(async (req, res) => {
+  const { slug } = req.params;
+  try {
+    const blogBySlug = await Blog.findOne({ slug });
+    res.json({
+      success: true,
+      message: "get blog by slug successfully",
+      data: blogBySlug,
+    });
+  } catch (error) {
+    throw new Error(error.message);
+  }
+});
 
 // export all blogs controller
+module.exports = {
+  createBlog,
+  getAllBlogs,
+  getBlogById,
+  getBlogBySlug,
+};
--- routes/v1/blogRoute.js
+++ routes/v1/blogRoute.js
@@ -2,8 +2,18 @@ const express = require("express");
 const router = express.Router();
 
 // import controllers
+const {
+  createBlog,
+  getAllBlogs,
+  getBlogById,
+  getBlogBySlug,
+} = require("../../controllers/blogController");
 
 // all routes
+router.post("/create-blog", createBlog);
+router.get("/get-all", getAllBlogs);
+router.get("/get-by-id/:id", getBlogById);
+router.get("/get-by-slug/:slug", getBlogBySlug);
 
 // export router
 module.exports = router;
