eb20b49afb919bea1dc38a58c8a07718e83a0dda
Md Mofizul Islam (Sohan) <mofizulislam0216@gmail.com>
2024-01-06 21:14:39 +0600

    Add category CRUD operations

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -24,7 +24,109 @@ const createCategory = asyncHandler(async (req, res) => {
   }
 });
 
+// get all categories controller
+const getAllCategories = asyncHandler(async (req, res) => {
+  try {
+    const categories = await Category.find({});
+    res.json({
+      success: true,
+      message: "get all categories successfully...",
+      data: categories,
+      meta: {
+        total: categories.length,
+      },
+    });
+  } catch (error) {
+    throw new Error(error.message);
+  }
+});
+
+// get category by id controller
+const getCategoryById = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongodbId(id);
+  try {
+    const category = await Category.findById(id);
+    if (!category) {
+      throw new Error("category not found...");
+    }
+    res.json({
+      success: true,
+      message: "get category by id successfully...",
+      data: category,
+    });
+  } catch (error) {
+    throw new Error(error.message);
+  }
+});
+
+// get category by slug controller
+const getCategoryBySlug = asyncHandler(async (req, res) => {
+  const { slug } = req.params;
+  try {
+    const category = await Category.findOne({ slug });
+    if (!category) {
+      throw new Error("category not found...");
+    }
+    res.json({
+      success: true,
+      message: "get category by slug successfully...",
+      data: category,
+    });
+  } catch (error) {
+    throw new Error(error.message);
+  }
+});
+
+// update category controller
+const updateCategory = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongodbId(id);
+  try {
+    const category = await Category.findById(id);
+    if (!category) {
+      throw new Error("category not found...");
+    }
+    const updatedCategory = await Category.findByIdAndUpdate(
+      id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.json({
+      success: true,
+      message: "update category successfully...",
+      data: updatedCategory,
+    });
+  } catch (error) {
+    throw new Error(error.message);
+  }
+});
+
+// delete category controller
+const deleteCategory = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongodbId(id);
+  try {
+    const category = await Category.findById(id);
+    if (!category) {
+      throw new Error("category not found...");
+    }
+    await Category.findByIdAndDelete(id);
+    res.json({
+      success: true,
+      message: "delete category successfully...",
+    });
+  } catch (error) {
+    throw new Error(error.message);
+  }
+});
+
 // exports all controllers
 module.exports = {
   createCategory,
+  getAllCategories,
+  getCategoryById,
+  getCategoryBySlug,
+  updateCategory,
+  deleteCategory,
 };
--- routes/v1/categoryRoute.js
+++ routes/v1/categoryRoute.js
@@ -2,13 +2,25 @@ const express = require("express");
 const router = express.Router();
 
 // import all controllers
-const { createCategory } = require("../../controllers/categoryController");
+const {
+  createCategory,
+  getAllCategories,
+  getCategoryById,
+  getCategoryBySlug,
+  updateCategory,
+  deleteCategory,
+} = require("../../controllers/categoryController");
 
 // auth middleware will apply when project is ready for production
 const { authMiddleware, isAdmin } = require("../../middlewares/authMiddleware");
 
 // create new category route
-router.post("/create-category", createCategory);
+router.post("/create-category", createCategory); // admin portected route
+router.get("/get-all", getAllCategories);
+router.get("/get-category-by-id/:id", getCategoryById);
+router.get("/get-category-by-slug/:slug", getCategoryBySlug);
+router.put("/update-category/:id", updateCategory); // admin portected route
+router.delete("/delete-category/:id", deleteCategory); // admin portected route
 
 // export all routes
 module.exports = router;
