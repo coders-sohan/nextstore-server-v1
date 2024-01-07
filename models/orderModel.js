922ea069433e92d46b6c9cbb2018fb77d0d7ca0f
Md Mofizul Islam (Sohan) <mofizulislam0216@gmail.com>
2024-01-07 13:15:52 +0600

    Refactor code for improved performance and readability

---
+++ models/orderModel.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+// Declare the Schema of the Mongo model
+var orderSchema = new mongoose.Schema(
+  {
+    products: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+        },
+        count: Number,
+      },
+    ],
+    paymentIntent: {},
+    orderStatus: {
+      type: String,
+      default: "Not Processed",
+      enum: [
+        "Not Processed",
+        "Cash On Delivery",
+        "Processing",
+        "Dispatched",
+        "Cancelled",
+        "Completed",
+      ], // enum means string objects
+    },
+    orderedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//Export the model
+module.exports = mongoose.model("Order", orderSchema);
